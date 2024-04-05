e96e1c906f8b138fd277ebf3e7d167ca43fa40d9
Adrian <adrian.luque.rod@gmail.com>
2024-04-06 01:33:03 +0200

    dsa

---
+++ intro_java/src/main/java/_08colecciones/Cola.java
@@ -0,0 +1,13 @@
+package _08colecciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cola {
+    List<Persona> listaPersonas;
+
+    public Cola() {
+        listaPersonas = new ArrayList<>();
+    }
+
+}
