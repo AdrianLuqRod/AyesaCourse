1fdefd4dd246eea942a2e1c85833d168f0199159
Adrian <adrian.luque.rod@gmail.com>
2024-04-06 12:55:06 +0200

    sda

--- intro_java/src/main/java/_08colecciones/Cola.java
+++ intro_java/src/main/java/_08colecciones/Cola.java
@@ -10,4 +10,21 @@ public class Cola {
         listaPersonas = new ArrayList<>();
     }
 
+    public List<Persona> getListaPersonas() {
+        return listaPersonas;
+    }
+
+    public void setListaPersonas(List<Persona> listaPersonas) {
+        this.listaPersonas = listaPersonas;
+    }
+
+    public void nuevoCliente(Persona persona) {
+        listaPersonas.add(persona);
+    }
+
+    public void avanzarCola() {
+        if (listaPersonas.size() > 0) {
+            listaPersonas.remove(0);
+        }
+    }
 }
