8f35d1a6f26ba0099b553089c388e9b7b21dfcef
Adrian Luque Rodriguez <aluquer@ayesa.es>
2024-05-09 13:51:16 +0200

    fin js

--- curso_java/ej01/ej01.js
+++ curso_java/ej01/ej01.js
@@ -1,3 +1,20 @@
-function holaConsola() {
-  console.log("Hola Mundo");
+function pideDevuelve() {
+  let primerNumero = prompt("Introduce un número");
+  primerNumero = parseInt(primerNumero);
+  return primerNumero;
+}
+
+function tipoTrato(primerNumero) {
+  let trato = confirm("¿Quieres un trato especial?");
+  if (trato) {
+    return primerNumero + 10;
+  } else {
+    return primerNumero - 5;
+  }
+}
+
+function main() {
+  let primerNumero = pideDevuelve();
+  let resultado = tipoTrato(primerNumero);
+  alert("El resultado es: " + resultado);
 }
